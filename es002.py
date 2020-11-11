1e20e66aeac19710385d7d5873c31bb57529b25b
Alexandre Rodrigues <alexandreemi@gmail.com>
2020-11-11 20:30:58 -0300

    teste1

---
+++ es002.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um numero'))
+n2 = int(input('Digit outro numero'))
+s = n1 + n2
+#print('a soma entre', n1 ,'e', n2, 'é',s)
+print(f'A soma dos Numeros {n1} e {n2} é igual a:{s}')
+
